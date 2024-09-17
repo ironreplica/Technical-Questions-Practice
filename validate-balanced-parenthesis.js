dd7a59ae9a9cb31be83c84382c580659d62d3cb5
ironreplica <trevorchilds8@gmail.com>
2024-09-17 12:29:37 -0600

    removed console log

--- validate-balanced-parenthesis.js
+++ validate-balanced-parenthesis.js
@@ -4,8 +4,6 @@ var input2 = "({[([)]]})";
 
 function isBalancedBrackets(input) {
   var inputArr = input.split("");
-  console.log(inputArr.length);
-
   for (var i = 0; i <= inputArr.length + 1; i++) {
     if (
       (inputArr[0] == "(" && inputArr[inputArr.length - 1] == ")") ||
@@ -14,7 +12,6 @@ function isBalancedBrackets(input) {
     ) {
       inputArr.splice(0, 1);
       inputArr.splice(inputArr.length - 1, 1);
-      console.log(inputArr.length);
     }
   }
 
